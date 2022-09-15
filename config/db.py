88282b76e16c30304f7837f20ce9c67ed5ed32c9
MarielCG <cochachi.g.m@gmail.com>
2022-09-14 19:01:21 -0500

    modified:   config/__pycache__/db.cpython-310.pyc 	modified:   config/db.py

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine('mysql+pymysql://sql10519779:GCQw6lAdZU@sql10.freemysqlhosting.net/:3306/lab_individual')
+engine = create_engine('mysql+pymysql://sql10519779:GCQw6lAdZU@sql10.freemysqlhosting.net:3306/sql10519779')
 #engine = create_engine('mysql+pymysql://root:Clave1601**@localhost:3306/lab_individual')
 
 meta = MetaData()
